9bc40184b68c52d7b70e34f653875fcacd731f05
Eyerus Molla <eyerusmolla29@gmail.com>
2025-02-26 12:14:30 -0800

    initial commit message

---
+++ script.js
@@ -0,0 +1,54 @@
+let startTime, updatedTime, difference, tInterval;
+let running = false;
+const display = document.getElementById('display');
+const lapTimes = document.getElementById('lapTimes');
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(updateTime, 1000);
+        running = true;
+    }
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // Format time as HH:MM:SS
+    display.innerHTML = (hours < 10 ? "0" + hours : hours) + ":" +
+                        (minutes < 10 ? "0" + minutes : minutes) + ":" +
+                        (seconds < 10 ? "0" + seconds : seconds);
+}
+
+function pauseTimer() {
+    clearInterval(tInterval);
+    running = false;
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    difference = 0;
+    display.innerHTML = "00:00:00";
+    lapTimes.innerHTML = ""; // Clear lap times
+}
+
+function recordLap() {
+    if (running) {
+        const lapTime = display.innerHTML;
+        const lapElement = document.createElement('div');
+        lapElement.className = 'lap';
+        lapElement.innerText = "Lap: " + lapTime;
+        lapTimes.appendChild(lapElement);
+    }
+}
+
+document.getElementById('start').addEventListener('click', startTimer);
+document.getElementById('pause').addEventListener('click', pauseTimer);
+document.getElementById('reset').addEventListener('click', resetTimer);
+document.getElementById('lap').addEventListener('click', recordLap);
